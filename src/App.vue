3b3578debd195e9ba86f5f9585e28141fdb97ad4
ATMJin <atmjin0912@protonmail.com>
2022-06-30 23:45:56 +0800

    stash

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import CustomShapeButton from '@/components/CustomShapeButton.vue';
+import DashboardSidebarMenu from '@/components/DashboardSidebarMenu.vue';
 </script>
 
 <template>
   <header>
     <div class="wrapper">
       <CustomShapeButton></CustomShapeButton>
+      <DashboardSidebarMenu></DashboardSidebarMenu>
     </div>
   </header>
 
---
+++ src/components/DashboardSidebarMenu.vue
@@ -0,0 +1,61 @@
+<template >
+  <div class="body">
+    <div class="navigation">
+      <ul>
+        <li v-for="(item, index) in items" :key="index">
+          <a href="#">
+            <span class="icon"></span>
+            <span class="title"></span></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue';
+let items = reactive([{
+  icon: '',
+  title: 'Brand Name',
+}, {
+  icon: '',
+  title: 'Dashboard',
+}, {
+  icon: '',
+  title: 'Customers',
+}, {
+  icon: '',
+  title: 'Messagers',
+}, {
+  icon: '',
+  title: 'Help',
+}, {
+  icon: '',
+  title: 'setting',
+}, {
+  icon: '',
+  title: 'password',
+}, {
+  icon: '',
+  title: 'Sign Out',
+},]);
+
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+.body {
+  min-height: 100vh;
+  background: #365fa1;
+
+  .navigation {
+    position: fixed;
+    background: #000;
+  }
+}
+</style>
